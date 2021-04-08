b984c141533a1fde4aaa97bec683ed5b8fa45e16
alekseevayana <alekseevayana@bk.ru>
2021-04-08 14:19:48 -0700

    add storage and graphql

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />
