e6a9f84a0d7e412c70787f5dfdace2f2ffdc7c46
alekseevayana <alekseevayana@bk.ru>
2021-04-12 15:20:44 -0700

    sticky footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import MainPage from './pages/MainPage';
 function App() {
   
   return (
-    <div className="App">
+    <div className="app">
       <MainPage/>
     </div>
   );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,14 +5,14 @@ import './MainPage.css'
 export default function MainPage() {
     return (
         <div className="main-page">
-            <section className="main-page_navigation">
+            <section className="main-page_item main-page_navigation">
                 <Navigation/>
             </section>
-            <div className="main-page_wrapper">
+            <div className="main-page_item main-page_wrapper">
                 <section className="main-page_top-block">TOP Block</section>
                 <section className="main-page_twits">Twits</section>
             </div>
-            <section className="main-page_footer">
+            <section className="main-page_item main-page_footer">
                 Footer
             </section>
         </div>
